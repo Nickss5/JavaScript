ea73d79ee9cd52f53bfb8340f6b4e5ff4f34cdb7
Nishma_M <medagoninishma@gmail.com>
2024-10-25 00:44:05 +0000

    Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -47,4 +47,59 @@ let stringNumber = 4.44
 let conStringNumber = String(stringNumber)
 console.log(conStringNumber) // 33 - string
 
+//----------------- operations --------------------------
+
+let value = 33
+let negValue = -value // -33
+
+//arthemetic operators --> +, -, *, %, /, **
+
+//concat 
+let str1 = "Nishma"
+let str2 = "Hello "
+
+let full = str2 + str1 // Hello Nishma
+
+// Different types of concatination
+
+console.log(2+2)  // --> 4 - number
+console.log("2" + "2") // --> 22 - string
+console.log(1 + "2") // --> 12 - string
+console.log("1" + 2) // --> 12 - string
+console.log("1" + 2 + 2) // --> 122 - string - one string then entirity is string
+console.log(1 + 2 + "2") // --> 32 - string is last 
+
+// Another type of conversion (not a good practice)
+
+console.log(+true) // 1 --> converted to number boolean 
+console.log(+"") //  0 --> since empty string is always false/0
+
+// Prefix and Suffix operands
+
+// 1) Prefix -- (++x) --> increments and returns the value after incrementing
+
+let x = 3
+let y = ++x 
+console.log(`x: ${x}, y: ${y}`)
+// output :  x: 4, y: 4
+
+//----------------------------------------
+
+// 2) Suffix/Postfix -- (x++) --> returns the value then increments the value
+
+let a = 3
+let b = a++
+console.log(`a: ${a}, b: ${b}`)
+//output : a: 4, b: 3
+
+//------------ when does the increment not change for pre/postfix ------------
+
+let gameCounter = 100
+++gameCounter // 101
+gameCounter // 101
+
+
+
+
+
 
